099d63351af778879c155334a66451625b5f9d74
Alexander Ivanov <Mossd925@gmail.com>
2024-10-24 15:43:50 +0300

    add library entry file

---
+++ lib/models/platform_dependent_services.dart
@@ -0,0 +1,4 @@
+library platform_dependent_services;
+
+export 'package:platform_dependent_services/models/message_model.dart';
+export 'package:platform_dependent_services/platform_dependencies.dart';
